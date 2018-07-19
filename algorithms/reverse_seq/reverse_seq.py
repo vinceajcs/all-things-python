6d9ca150c2d897cf22b1b9b92e53b6baf7cd3635
vinceajcs <hvincentstephen@gmail.com>
2018-07-19 16:45:50 -0700

    Added reverse sequence algorithm example

---
+++ algorithms/reverse_seq/reverse_seq.py
@@ -0,0 +1,35 @@
+def reverse_seq(S, start, stop):
+    """Reverses n elements of a sequence S, using linear recursion.
+    Swaps first and last elements, then recursively reverses remaining elements."""
+    if start < stop - 1:
+        S[start], S[stop - 1] = S[stop - 1], S[start]
+        reverse_seq(S, start + 1, stop - 1)
+
+    return S
+
+
+test = [4, 3, 6, 2, 8, 9, 5]
+print(reverse_seq(test, 0, len(test)))  # test changes here
+
+
+def reverse_seq2(S):
+    copy = S[:]  # Creates copy
+    return copy[::-1]  # Slicing notation
+
+
+test = [4, 3, 6, 2, 8, 9, 5]
+print(reverse_seq2(test))
+
+print(test)  # test stays the same
+
+
+def reverse_seq_iterative(S):
+    start, stop = 0, len(S)
+    while start < stop - 1:
+        S[start], S[stop - 1] = S[stop - 1], S[start]
+        start, stop = start + 1, stop - 1
+
+    return S
+
+
+print(reverse_seq_iterative(test))
