0fdd469804206d6aebf18e9d706930517ccb2545
vinceajcs <hvincentstephen@gmail.com>
2018-07-26 16:56:59 -0700

    Added breadth first tree traversal

--- data_structures/tree/tree_traversals.py
+++ data_structures/tree/tree_traversals.py
@@ -1,3 +1,6 @@
+from collections import deque
+
+
 def preorder(tree):
     if tree:
         print(tree.get_root())
@@ -17,3 +20,16 @@ def postorder(tree):
         postorder(tree.get_left())
         postorder(tree.get_right())
         print(tree.get_root())
+
+
+def bft(tree):
+    queue = deque([tree.get_root()])
+    while queue is not None:
+        node = queue.popleft()
+        print(node)
+
+        if node.get_left() is not None:
+            queue.append(node.get_left())
+
+        if node.get_right() is not None:
+            queue.append(node.get_right())
