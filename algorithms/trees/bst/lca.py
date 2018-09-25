729b9d4f16ba415875c88c560bc3d8ef771f453e
vinceajcs <hvincentstephen@gmail.com>
2018-09-24 22:20:15 -0700

    update lca algorithm for bst

--- algorithms/trees/bst/lca.py
+++ algorithms/trees/bst/lca.py
@@ -17,21 +17,3 @@ def lowest_common_ancestor(root, p, q):
             root = root.right
         else:
             return root
-
-
-# recursive
-def lowest_common_ancestor(root, p, q):
-    if root in (None, p, q):
-        return root
-
-    left, right = [lowest_common_ancestor(child, p, q) for child in (root.left, root.right)]
-
-    """
-    1. If the current subtree contains both p and q,
-        return their LCA.
-    2. If only one of them is in that subtree,
-        return that one.
-    3. If neither of them is in that subtree,
-        return the node of that subtree.
-    """
-    return root if left and right else left or right
