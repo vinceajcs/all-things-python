f0a2d882013f951b2db3dbf21bd91dc6306bd65d
vinceajcs <hvincentstephen@gmail.com>
2018-09-27 14:13:16 -0700

    modify binary search

--- algorithms/binary_search/binary_search.py
+++ algorithms/binary_search/binary_search.py
@@ -27,14 +27,3 @@ def binary_search_recursive(data, target, low, high):
             return binary_search_recurisve(data, target, low, mid - 1)
         else:
             return binary_search_recursive(data, target, mid + 1, high)
-
-
-test = [1, 2, 5, 10, 20]
-x = 10
-y = 100
-
-print(binary_search(test, x))
-print(binary_search_recursive(test, x, 0, len(test) - 1))
-
-print(binary_search(test, y))
-print(binary_search_recursive(test, y, 0, len(test) - 1))
