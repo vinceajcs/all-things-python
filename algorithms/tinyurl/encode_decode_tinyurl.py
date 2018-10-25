af3b3b45e4ed98ae10786f7bb5e3294c7683e23c
vinceajcs <hvincentstephen@gmail.com>
2018-10-25 16:00:38 -0700

    add encode decode tinyURL

---
+++ algorithms/tinyurl/encode_decode_tinyurl.py
@@ -0,0 +1,38 @@
+"""Using increasing #s as codes."""
+
+
+class TinyURL:
+    def __init__(self):
+        self.urls = []
+
+    def encode(self, long_url):
+        self.urls.append(long_url)
+        return 'http://tinyurl.com/' + str(len(self.urls) - 1)
+
+    def decode(self, short_url):
+        return self.urls[int(short_url.split('/')[-1])]
+
+
+"""Better way, using a random code of six digit/letter combinations.
+If a longURL is already stored, use existing shortURL.
+"""
+
+
+class TinyURL:
+
+    alphabet = string.ascii_letters + string.digits
+
+    def __init__(self):
+        self.urls = {}  # url -> code
+        self.codes = {}  # code -> url
+
+    def encode(self, long_url):
+        if long_url not in self.urls:
+            code = ''.join(random.choice(TinyURL.alphabet) for _ in range(6))
+            if code not in self.codes:
+                self.codes[code] = long_url
+                self.urls[long_url] = code
+        return 'http://tinyurl.com/' + self.urls[long_url]
+
+    def decode(self, short_rl):
+        return self.codes[short_url[-6:]]
