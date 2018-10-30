84c93620f3b2947af1ac2ded7b613e00a1eedf0c
vinceajcs <hvincentstephen@gmail.com>
2018-10-30 10:24:18 -0700

    add roman to int

--- algorithms/string/roman_to_integer.py
+++ algorithms/string/roman_to_integer.py
@@ -3,3 +3,22 @@
 Time: O(n)
 Space: O(1)
 """
+
+
+def roman_to_integer(s):
+    lookup = {'M': 1000, 'D': 500 , 'C': 100, 'L': 50, 'X': 10,'V': 5,'I': 1}
+
+    prev = 0
+    result = 0
+
+    for i in range(len(s)-1, -1, -1):
+        current = lookup[s[i]]
+
+        if current < prev:
+            result -= current
+        else:
+            result += current
+
+        prev = current
+
+    return result
