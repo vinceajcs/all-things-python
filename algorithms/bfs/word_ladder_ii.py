edcbfe25ea76ec65e50ea38e4e0b78a04830ba98
vinceajcs <hvincentstephen@gmail.com>
2018-10-31 16:39:16 -0700

    modify word ladder ii

--- algorithms/bfs/word_ladder_ii.py
+++ algorithms/bfs/word_ladder_ii.py
@@ -2,28 +2,28 @@
 
 
 def word_ladder(begin_word, end_word, word_list):
-    wordList = set(wordList)
+    word_list = set(word_list)
     paths = []
 
     level = {}
-    level[beginWord] = [[beginWord]]
+    level[begin_word] = [[begin_word]]
 
     while level:
         next_level = collections.defaultdict(list)
 
         for word in level:
-            if word == endWord:
+            if word == end_word:
                 for path in level[word]:
                     paths.append(path)
             else:
                 for i in range(len(word)):
                     for c in string.ascii_lowercase:
                         next_word = word[:i] + c + word[i + 1:]
-                        if next_word in wordList:
+                        if next_word in word_list:
                             for path in level[word]:
                                 next_level[next_word].append(path + [next_word])
 
-        wordList -= set(next_level.keys())
+        word_list -= set(next_level.keys())
         level = next_level
 
     return paths
