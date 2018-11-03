4f2596b06d5083f804cf0a0caa45a0f03a18ab0d
vinceajcs <hvincentstephen@gmail.com>
2018-11-03 11:45:52 -0700

    modify inorder successor

--- algorithms/tree/binary_tree/bst/inorder_successor.py
+++ algorithms/tree/binary_tree/bst/inorder_successor.py
@@ -3,7 +3,7 @@
 Idea: Traverse the BST inorder.
 Since there are no duplicates in a BST, to find the successor, find the smallest element that is greater than the input node.
 
-Time: O(n)
+Time: O(h), where h = logn if the BST is balanced, otherwise h = n.
 Space: O(1)
 """
 
@@ -13,7 +13,7 @@ def inorder_successor(root, p):
     while root:
         if p.val < root.val:
             successor = root
-            root = root.left
+            root = root.left  # check left subtree to see if there is a value smaller than root.val
         else:
             root = root.right
     return successor
