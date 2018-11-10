cda48ea9c821c7c3462cd2f1a106feb180ae0030
vinceajcs <hvincentstephen@gmail.com>
2018-11-09 19:55:46 -0800

    add tic tac toe

--- design/tic_tac_toe.py
+++ design/tic_tac_toe.py
@@ -1,51 +1,6 @@
-"""Design a Tic-tac-toe game that is played between two players on a n x n grid.
+"""Design a Tic-Tac-Toe game that is played between two players on a n x n grid.
 
-You may assume the following rules:
-
-A move is guaranteed to be valid and is placed on an empty block.
-Once a winning condition is reached, no more moves is allowed.
-A player who succeeds in placing n of their marks in a horizontal, vertical, or diagonal row wins the game.
-Example:
-Given n = 3, assume that player 1 is "X" and player 2 is "O" in the board.
-
-TicTacToe toe = new TicTacToe(3);
-
-toe.move(0, 0, 1); -> Returns 0 (no one wins)
-|X| | |
-| | | |    // Player 1 makes a move at (0, 0).
-| | | |
-
-toe.move(0, 2, 2); -> Returns 0 (no one wins)
-|X| |O|
-| | | |    // Player 2 makes a move at (0, 2).
-| | | |
-
-toe.move(2, 2, 1); -> Returns 0 (no one wins)
-|X| |O|
-| | | |    // Player 1 makes a move at (2, 2).
-| | |X|
-
-toe.move(1, 1, 2); -> Returns 0 (no one wins)
-|X| |O|
-| |O| |    // Player 2 makes a move at (1, 1).
-| | |X|
-
-toe.move(2, 0, 1); -> Returns 0 (no one wins)
-|X| |O|
-| |O| |    // Player 1 makes a move at (2, 0).
-|X| |X|
-
-toe.move(1, 0, 2); -> Returns 0 (no one wins)
-|X| |O|
-|O|O| |    // Player 2 makes a move at (1, 0).
-|X| |X|
-
-toe.move(2, 1, 1); -> Returns 1 (player 1 wins)
-|X| |O|
-|O|O| |    // Player 1 makes a move at (2, 1).
-|X|X|X|
-
-
+Idea:
 Record the number of moves for each rows, columns, and two diagonals.
 For each move, we -1 for each player 1's move and +1 for player 2's move.
 Then we just need to check whether any of the recorded numbers equal to n or -n.
