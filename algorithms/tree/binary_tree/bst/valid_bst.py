3cf3c5571da0dcf3d7c7094bbad7774f6eb8b456
vinceajcs <hvincentstephen@gmail.com>
2018-11-09 20:06:16 -0800

    modify valid bst

--- algorithms/tree/binary_tree/bst/valid_bst.py
+++ algorithms/tree/binary_tree/bst/valid_bst.py
@@ -19,9 +19,12 @@ def is_valid_bst(root):
         while root:
             stack.append(root)
             root = root.left
-        root = stack.pop()
-        if pre and root.val <= pre.val:
+
+        root = stack.pop()  # start with leftmost element
+        if pre and root.val <= pre.val:  # BST condition check
             return False
+
+        # continue traversal
         pre = root
         root = root.right
 
