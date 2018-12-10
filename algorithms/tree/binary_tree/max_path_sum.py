3585478f53d6cb4fdcbafb0a71324bf987b82bfd
vinceajcs <hvincentstephen@gmail.com>
2018-12-10 10:38:01 -0800

    add examples to max path sum

--- algorithms/tree/binary_tree/max_path_sum.py
+++ algorithms/tree/binary_tree/max_path_sum.py
@@ -2,6 +2,26 @@
 A path is defined as any sequence of nodes from some starting node to any node in the tree along the parent-child connections.
 The path must contain at least one node and does not need to go through the root.
 
+Example 1:
+Input: [1,2,3]
+
+       1
+      / \
+     2   3
+
+Output: 6 (1 + 2 + 3)
+
+Example 2:
+Input: [-10,9,20,null,null,15,7]
+
+   -10
+   / \
+  9  20
+    /  \
+   15   7
+
+Output: 42 (20 + 15 + 7)
+
 Time: O(n)
 Space: O(h), where h is the height of the binary tree
 """
