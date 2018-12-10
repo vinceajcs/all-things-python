ce2f1da8d0199dcea9d224b34ff4c38283684602
vinceajcs <hvincentstephen@gmail.com>
2018-12-10 10:32:18 -0800

    modify max depth iterative solution

--- algorithms/tree/binary_tree/max_depth.py
+++ algorithms/tree/binary_tree/max_depth.py
@@ -18,7 +18,8 @@ def max_depth(root):
     if not root:
         return 0
 
-    max_depth, level, depths = 0, [root], [1]
+    max_depth = 0
+    level, depths = [root], [1]
 
     while level:
         next_level = []
@@ -26,13 +27,11 @@ def max_depth(root):
         max_depth = max_depth if max_depth > depth else depth
 
         for node in level:
-            left, right = node.left, node.right
-
-            if left:
-                next_level.append(left)
+            if node.left:
+                next_level.append(node.left)
                 depths.append(depth + 1)
-            if right:
-                next_level.append(right)
+            if node.right:
+                next_level.append(node.right)
                 depths.append(depth + 1)
 
         level = next_level
