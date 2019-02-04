a49682ccfa2e71af84dfd77e70c9ee45bd884bda
vinceajcs <hvincentstephen@gmail.com>
2019-02-04 14:34:16 -0800

    add dfs solution for invert binary tree

--- algorithms/tree/binary_tree/invert_binary_tree.py
+++ algorithms/tree/binary_tree/invert_binary_tree.py
@@ -7,22 +7,46 @@ Space: O(n)
 
 # recursive
 def invert_binary_tree(root):
-    if root is not None:
+    if root:
         root.left, root.right = inverse_binary_tree(root.right), inverse_binary_tree(root.left)
     return root
 
 
-# iterative
+# iterative - BFS
 def invert_binary_tree(root):
-    if root:
-        nodes = collections.deque([])
-        nodes.append(root)
+    if not root:
+        return root
 
-        while nodes:
-            node = nodes.popleft()
-            node.left, node.right = node.right, node.left
-            if node.left:
-                nodes.append(node.left)
-            if node.right:
-                nodes.append(node.right)
+    queue = collections.deque([root])
+
+    while queue:
+        node = queue.popleft()
+
+        node.left, node.right = node.right, node.left
+
+        if node.left:
+            queue.append(node.left)
+        if node.right:
+            queue.append(node.right)
+
+    return root
+
+
+# iterative - DFS
+def invert_binary_tree(root):
+    if not root:
+        return root
+
+    stack = [root]
+
+    while stack:
+        node = stack.pop()
+
+        node.left, node.right = node.right, node.left
+
+        if node.right:
+            stack.append(node.right)
+        if node.left:
+            stack.append(node.left)
+
     return root
