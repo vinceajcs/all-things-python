b01714d0708ba98e2d86cf56bb6f98721390dec3
vinceajcs <hvincentstephen@gmail.com>
2019-02-05 10:48:40 -0800

    modify k closest points to origin

--- algorithms/heap/k_closest_points.py
+++ algorithms/heap/k_closest_points.py
@@ -13,16 +13,21 @@ def naive(points, k, origin=(0, 0)):
     return sorted(points, key=lambda p: distance(p, origin))[:k]
 
 
-"""Better solution: the idea here is to maintain a max heap with the k closest points.
+"""Better solution.
+
+Idea: Maintain a max heap of k elements.
+We can iterate through all points.
+If a point p has a smaller distance to the origin than the top element of a heap, we add point p to the heap and remove the top element.
+After iterating through all points, our heap contains the k closest points to the origin.
 
 Time: O(nlogk)
 Space: O(k)
 """
 
 
-def closest(points, k, origin=(0, 0)):
-    """Create max heap with first k points.
-    Python does not support a max heap; thus we can use a min heap where the keys are negated
+def k_closest(points, k, origin=(0, 0)):
+    """Initialize max heap with first k points.
+    Python does not support a max heap; thus we can use the default min heap where the keys (distance) are negated.
     """
     heap = [(-distance(p, origin), p) for p in points[:k]]
     heapify(heap)
@@ -30,20 +35,24 @@ def closest(points, k, origin=(0, 0)):
     """
     For every point p in points[k:],
     check if p is smaller than the root of the max heap;
-    if it is, remove root and add p to heap. Reheapify.
+    if it is, add p to heap and remove root. Reheapify.
     """
     for p in points[k:]:
         d = distance(p, origin)
 
         heappushpop(heap, (-d, p))  # heappushpop does conditional check
-        """same as:
+        """Same as:
             if d < -heap[0][0]:
                 heappush(heap, (-d,p))
                 heappop(heap)
+
+        Note: heappushpop is more efficient than separate push and pop calls.
+        Each heappushpop call takes O(logk) time.
         """
 
     return [p for nd, p in heap]  # return points in heap
 
 
 """Another idea: find kth element, and return all elements less than that element.
-This can be done in linear time, using quickselect."""
+This can be done in linear time, using quickselect.
+"""
