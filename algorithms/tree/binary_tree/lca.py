1ee8568c722ad64f39d716e46e65463805a9f495
vinceajcs <hvincentstephen@gmail.com>
2019-02-05 20:45:13 -0800

    add iterative solution for lowest common ancestor of binary tree

--- algorithms/tree/binary_tree/lca.py
+++ algorithms/tree/binary_tree/lca.py
@@ -1,13 +1,5 @@
 """Given a binary tree, find the lowest common ancestor of two given nodes in the binary tree.
 
-Idea:
-1. If the current subtree contains both p and q,
-    return their LCA.
-2. If only one of them is in that subtree,
-    return that one.
-3. If neither of them is in that subtree,
-    return the node of that subtree.
-
 Time: O(n)
 Space: O(h)
 """
@@ -18,6 +10,44 @@ def lowest_common_ancestor(root, p, q):
     if root in (None, p, q):
         return root
 
-    left, right = [lowest_common_ancestor(child, p, q) for child in (root.left, root.right)]
+    # search subtrees
+    left = lowest_common_ancestor(root.left, p, q)
+    right = lowest_common_ancestor(root.right, p, q)
 
-    return root if left and right else left or right
+    if left and right:  # both are found in the subtree, root is LCA
+        return root
+
+    return left or right  # either root of left or right subtree is LCA
+
+
+# iterative
+def lowest_common_ancestor(root, p, q):
+    if not root:
+        return root
+
+    stack = [root]
+    parents = {root: None}
+
+    # keep track of parents
+    while p not in parents or q not in parents:
+        node = stack.pop()
+
+        if node.left:
+            parents[node.left] = node
+            stack.append(node.left)
+        if node.right:
+            parents[node.right] = node
+            stack.append(node.right)
+
+    ancestors = set()
+
+    # get all of p's ancestors
+    while p:
+        ancestors.add(p)
+        p = parents[p]
+
+    # go through q's ancestors to see if any of them is an ancestor of p
+    while q not in ancestors:
+        q = parents[q]
+
+    return q
