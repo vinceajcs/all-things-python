f608b79b7b3fa6e1fab4c271ecd768787d20cf7c
vinceajcs <hvincentstephen@gmail.com>
2019-02-05 17:45:53 -0800

    modify convert bst to greater tree

--- algorithms/tree/binary_tree/bst/convert_bst_to_greater_tree.py
+++ algorithms/tree/binary_tree/bst/convert_bst_to_greater_tree.py
@@ -1,15 +1,15 @@
 """Given a BST, convert it to a Greater Tree such that every key of the original BST is changed to the original key plus the sum of all keys greater than the original key.
 
 Example:
-Input: The root of a Binary Search Tree like this:
-              5
-            /   \
-           2     13
+Input:
+          5
+        /   \
+       2     13
 
-Output: The root of a Greater Tree like this:
-             18
-            /   \
-          20     13
+Output:
+         18
+        /   \
+      20     13
 
 Idea: We can traverse the BST in reverse inorder.
 
