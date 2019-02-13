c006768aa4c5f85046e5e04ef37a34821c274e9d
vinceajcs <hvincentstephen@gmail.com>
2019-02-12 19:01:01 -0800

    modify meeting rooms ii

--- algorithms/array/interval/meeting_rooms_ii.py
+++ algorithms/array/interval/meeting_rooms_ii.py
@@ -1,7 +1,14 @@
 """Given an array of meeting time intervals consisting of start and end times [[s1,e1],[s2,e2],...] (si < ei), find the minimum number of conference rooms required.
 
+Example 1:
+Input: [[0, 30],[5, 10],[15, 20]]
+Output: 2
 
-We can use a min heap to store end times of each interval.
+Example 2:
+Input: [[7,10],[2,4]]
+Output: 1
+
+Idea: We can use a min heap to store end times of each interval.
 The room at the top of the min heap is then the one that will be free at the earliest time.
 If the room at the top is not free, no other room is. In this case, we allocate a new room.
 
@@ -21,7 +28,7 @@ def min_meeting_rooms(intervals):
     for i in intervals:
         if heap and i.start >= heap[0]:
             # means two intervals can use the same room
-            heapq.heapreplace(heap, i.end)
+            heapq.heapreplace(heap, i.end)  # pop then push
         else:
             # a new room is allocated
             heapq.heappush(heap, i.end)
